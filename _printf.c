c95b642a23c41c3dbdaee3553d0eb8e4c5dc7dbe
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-07-24 16:50:33 +0300

    Adding input validation

--- _printf.c
+++ _printf.c
@@ -28,7 +28,9 @@ int _printf(const char *format, ...)
 				{NULL, NULL}
 				};
 
-	if (!format)
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 
 	va_start(args, format);
